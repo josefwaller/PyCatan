79166c87f742934e62b8dd5cf6f17a7c93c20b6d
josefwaller <josef@siriusapplications.com>
2016-10-16 18:37:25 -0700

    Fixed bugs; Now working up to building phase

--- setup.py
+++ setup.py
@@ -4,8 +4,15 @@ setup(name='pycatan',
       version='0.1',
       description='A Python Module for playing The Settlers of Catan',
       url='https://github.com/josefwaller/PyCatan',
+      long_description=open("readme.md").read(),
       author='Josef Waller',
       author_email='josef@siriusapplications.com',
       license='MIT',
+      install_requires=[
+            'math',
+            'random',
+            'pprint',
+            'json'
+      ],
       packages=['PyCatan'],
       zip_safe=False)
